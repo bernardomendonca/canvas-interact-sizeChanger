40facdfea73aa68d21601a64fa1abf8bb6465d9f
bernardomendonca <bernardomendonca@outlook.com>
2018-06-13 17:11:51 +1000

    Added resizing and bug fixing

--- canvas.js
+++ canvas.js
@@ -1,80 +1,38 @@
-var canvas = document.querySelector("canvas");
+// SETTING UP THE CANVAS
 
+var canvas = document.querySelector("canvas");
 canvas.width =window.innerWidth;
 canvas.height =window.innerHeight;
-
 var c = canvas.getContext('2d');
 
-// c.fillStyle = "rgba(255, 0, 0, 0.5)";
-// // c.fillRect (x, y, width, height)
-// c.fillRect (100, 100, 100, 100);
-// c.fillStyle = "rgba(0, 0, 255, 0.5)";
-// c.fillRect (400, 100, 100, 100);
-// c.fillRect (400, 300, 100, 100);
-
-//  c = CONTEXT
-
-// LINE
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.strokeStyle = "#aaaaaa";
-// c.stroke();
-
-// // ARC / CIRCLE
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = "blue";
-// c.stroke();
-
-
-// for (var i = 0; i < 1000; i++) {
-// 	var x = Math.random() * window.innerWidth;
-// 	var y = Math.random() * window.innerHeight;
-// 	// Math.random gives a value between 0 and 1.
-// 	c.beginPath();
-// 	c.arc(x, y, 30, 0, Math.PI * 2, false);
-// 	c.strokeStyle = "blue";
-// 	c.stroke();
-// };
-
-
-// Creating the MOUSE object, with X and Y coordinates
+// VARIABLES
 var mouse = {
 	x: undefined,
-	y: undefined
-};
-
+	y: undefined};
 var maxRadius = 40;
 var minRadius = 2;
-
 var colorArray = [
-	'#f6f6f6',
-	'#a2a2a2',
-	'#g2g2g2',
-	'#e1e1e1',
-	'#b5b5b5'
-];
+	'#A3B5BF',
+	'#6C787F',
+	'#D9F2FF',
+	'#363D40',
+	'#C3D9E5'];
+// HESHA BLUES
+var circleArray = [];
 
+// EVENT LISTENERS
 window.addEventListener('mousemove', function(event){
 	// Extracting the mouse coordinates
 	mouse.x = event.x;
 	mouse.y = event.y;
 });
+window.addEventListener('resize', function(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	init();
+})
 
-
-// // Generating Random Color, in a gray(ish)scale
-// function randomColor(){
-// 	//pick a "red" from 0 to 100
-// 	var r = Math.floor(Math.random() * 100);
-// 	//pick a "green" from 0 to 100
-// 	var g = Math.floor(Math.random() * 100);
-// 	//pick a "blue" from 0 to 100
-// 	var b = Math.floor(Math.random() * 100);
-// 	// Using weights to guarantee the gray(ish)scale
-// 	"rgb(r, g, b)"
-// 	return "rgb(" + (r)*0.3 + ", " + (g)*0.59 + ", " + (b)*0.11 + ")";
-// }
+// FUNCTIONS
 
 function Circle(x, y, dx, dy, radius){
 	this.x = x;
@@ -122,16 +80,17 @@ function Circle(x, y, dx, dy, radius){
 	}
 }
 
-var circleArray = [];
-
-for (var i = 0; i < 100; i++) {
-	var radius = Math.random() * 3 + 1;
-	// To make sure that the circles wont be stuck in the borders:
-	var x = Math.random() * (innerWidth - radius * 2) + radius;
-	var y = Math.random() * (innerHeight - radius * 2) + radius;
-	var dx = (Math.random() - 0.5) * 3;
-	var dy = (Math.random() - 0.5) * 3;
-	circleArray.push(new Circle(x, y, dx, dy, radius));
+function init() {
+	circleArray = [];
+	for (var i = 0; i < 800; i++) {
+		var radius = Math.random() * 3 + 1;
+		// To make sure that the circles wont be stuck in the borders:
+		var x = Math.random() * (innerWidth - radius * 2) + radius;
+		var y = Math.random() * (innerHeight - radius * 2) + radius;
+		var dx = (Math.random() - 0.5) * 3;
+		var dy = (Math.random() - 0.5) * 3;
+		circleArray.push(new Circle(x, y, dx, dy, radius));
+	}	
 }
 
 function animate() {
@@ -144,4 +103,7 @@ function animate() {
 	}
 };
 
+// INIT + ANIMATE
+
+init();
 animate();
